8b788fa03a13d0095dcb6f7f09dc78323c5abc0f
v.popova <violipopova@gmail.com>
2025-06-04 00:14:55 +0300

    updated firebase config

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAp1-yM7MrprDnD74HaC_dQYhRsnmKiwYg",
-  authDomain: "localbites-7a650.firebaseapp.com",
-  projectId: "localbites-7a650",
-  storageBucket: "localbites-7a650.appspot.app",
-  messagingSenderId: "819609786792",
-  appId: "1:819609786792:web:deff0a94f80f84515ae59d"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
